d03e119450a7eee2e4b2236e7b01742710c6d278
Dag Thomas Olsen <dag.thomas.olsen@simployer.com>
2023-04-21 20:59:12 +0200

    Refractored prompts, new features
    Added qdrant.as_retriever with get_relevant_documents

---
+++ fastapi/submodules/prompts.py
@@ -0,0 +1,13 @@
+def documentSearch(prompt, docs):
+    template = f"""You are given the following extracted parts of a long document and a question, create a final answer with references.
+	If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+	If you know the answer ALWAYS return the sources in the answer.
+    
+    Identify the language and write your answer in the indetified language. Do not output the identified language.
+	=========
+	QUESTION: {prompt}
+	=========
+	CONTENT: {docs}
+	=========
+	FINAL ANSWER:"""
+    return template
